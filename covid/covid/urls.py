0f4c998a00285a1a359ad0ebcf93265b0a055c24
ScriptSmith <ScriptSmith@users.noreply.github.com>
2020-03-14 21:35:26 +1000

    Remove landing page and redirect to login

--- covid/covid/urls.py
+++ covid/covid/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include
 from django.urls import re_path
+from django.views.generic import RedirectView
 
 from rdrf.urls import urlpatterns as rdrf_urlpatterns
 
@@ -8,5 +9,6 @@ rdrf_urls = [p for p in rdrf_urlpatterns if not getattr(p, 'app_name', None) == 
 urlpatterns = [
     # Any custom URLs go here before we include the TRRF urls
     re_path(r'^api/v1/', include(('covid.services.rest.urls.api_urls', 'api_urls'), namespace='v1')),
+    re_path(r'^$', RedirectView.as_view(url='/account/login', permanent=False)),
     re_path(r'', include(rdrf_urls)),
 ]
