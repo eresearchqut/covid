863693c5ba6e496876a249c1d5a01cfe9c5c6896
Tamas Szabo <szabtam@gmail.com>
2020-03-16 10:22:45 +0800

    Project logo fix + remove unused static files

--- covid/covid/settings.py
+++ covid/covid/settings.py
@@ -20,7 +20,7 @@ SEND_ACTIVATION_EMAIL = False
 PROJECT_TITLE = env.get("project_title", "COVID Clinical Registry Platform")
 # PROJECT_TITLE_LINK = "login_router"
 
-PROJECT_LOGO = env.get("project_logo", "images/covid.png")
+PROJECT_LOGO = env.get("project_logo", "images/covid/project_logo.png")
 
 LOGIN_REDIRECT_URL = '/patientslisting'
 
